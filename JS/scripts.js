1b904cd3fc5940b7da9a73e5ae6849bad79c5dac
Yoan Bourassa <2442228@carrefour.cegepvicto.ca>
2024-12-03 10:15:02 -0500

    version 2.0

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,6 +3,8 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let essaie = 0;
+let trouve = 0;
 
 function flipCard() {
   if (lockBoard) return;
@@ -14,12 +16,14 @@ function flipCard() {
     // first click
     hasFlippedCard = true;
     firstCard = this;
+    essaie++;
 
     return;
   }
 
   // second click
   secondCard = this;
+  essaie++;
 
   checkForMatch();
 }
@@ -33,7 +37,7 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-
+  trouve++;
   resetBoard();
 }
 
@@ -61,3 +65,62 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+// début de mes modifications
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+const nePlusAfficher = document.getElementById("nePlusAfficher");
+const jeu = document.querySelector("memory-card");
+const nbEssaie = document.getElementById("essaie");
+const nbTemps = document.getElementById("temps");
+const nbRecord = document.getElementById("record");
+
+
+// Le bouton "Fermer" ferme le dialogue
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+function NePlusAfficher() {
+  dialog.close();
+}
+
+// Ouvre le dialog au début
+window.onload = function() {
+  var dialog = document.getElementById("dialog");
+  dialog.showModal();
+}
+
+cards.forEach(card => card.addEventListener("click", Donnee));
+
+let temps = 0;
+setInterval(Temps, 1000);
+let recordEssaie = 9999999999999;
+let recordTemps = 9999999999999;
+
+function Donnee() {
+  nbEssaie.innerText = ("Cartes tournées : " + essaie);
+ 
+  if (trouve == 6) {
+    Fin();
+  }
+}
+
+function Temps () {
+  if (essaie > 0) {
+    temps++;
+  }
+  
+  nbTemps.innerText = ("Temps : " + temps + "s");
+}
+
+function Fin() {
+  if (essaie < recordEssaie) {
+    recordEssaie = essaie;
+    recordTemps = temps;
+  }
+  nbRecord.innerText = ("Record : " + recordEssaie + " cartes tournées en " + recordTemps + "s");
+  
+}
